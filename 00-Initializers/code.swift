eae450875718e227000ebdd4d8282aa84e1c946a
Harold Ramos <chattingharold@gmail.com>
2018-08-05 17:24:56 -0400

    Updating

---
+++ 00-Initializers/code.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+// Initializers: long way
+var string = String.init() // ""
+var integer = Int.init() // 0
+var boolean = Bool.init() // false
+
+
+// Initializers: short way
+var string2 = String() // ""
+var integer2 = Int() // ""
+var boolean2 = Bool()
+
