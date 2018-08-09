4852ba8d3e022eba46803addface2107f82625af
Harold Ramos <chattingharold@gmail.com>
2018-08-08 22:58:24 -0400

    Updating basic classes

--- 18-Classes/00-base_class/code.swift
+++ 18-Classes/00-base_class/code.swift
@@ -8,9 +8,16 @@ class Person {
         self.name = name
         self.age = age
     }
+    
+    var showInfo: String {
+        return "My name is \(name) and I am \(34) years old."
+    }
 }
 
 var harold = Person(name: "Harold Ramos", age: 34)
 
 harold.name // Harold Ramos
 harold.age  // 34
+harold.showInfo // My name is Harold Ramos and I am 34 years old.
+
+
