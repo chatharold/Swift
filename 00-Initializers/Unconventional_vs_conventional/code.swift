ac9bfdcfa763335dfad990220e7de1cedab6a2fb
Harold Ramos <chattingharold@gmail.com>
2018-08-20 01:14:52 -0400

    Uncon_vs_Con declarations

---
+++ 00-Initializers/Unconventional_vs_conventional/code.swift
@@ -0,0 +1,19 @@
+// Unconventional way
+var oldArrayDeclaredAndInit: Array<Int> = [1, 2, 3]
+
+var oldArrayEmptyValue = Array<Int>()
+
+var oldDictionaryAndInit: Dictionary<String, Int> = ["One": 1, "Two": 2]
+
+var oldDictionaryEmptyValue = Dictionary<String, Int>()
+
+
+
+// Conventional way
+var newArrayDeclaredAndInit: [Int] = [1, 2, 3]
+
+var newArrayEmptyValue = [Int]()
+
+var newDictionaryAndInit: [String: Int] = ["One": 1, "Two": 2]
+
+var newDictionaryEmptyValue = [String: Int]()
