0bb5b6c6ad5a6d80a34fa21ca820fe194f66707a
Harold Ramos <chattingharold@gmail.com>
2018-09-03 20:26:18 -0400

    Logical operators

---
+++ 25-Logical-operators/code.swift
@@ -0,0 +1,15 @@
+true == true    // true
+false == true   // false
+true == false   // false
+
+true != true    // false
+false != true   // true
+
+true && true    // true
+true && false   // false
+
+true || true    // true
+true || false   // true
+
+!true           // false
+!false          // true
