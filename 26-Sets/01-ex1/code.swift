63dd803543487c87461e278bed5814f635b48e36
Harold Ramos <chattingharold@gmail.com>
2018-09-11 20:24:42 -0400

    Sets

---
+++ 26-Sets/01-ex1/code.swift
@@ -0,0 +1,14 @@
+// First Set: "random order "
+var colors = Set<String>()
+colors = ["Red", "Green", "Blue"]
+
+
+
+// Second Set: "not duplicates"
+var colors2 = Set([
+        "white",
+        "yellow",
+        "green",
+        "yellow", // repeted
+        "white" // repeated
+])
