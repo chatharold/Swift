21223ca74225604d1050cc2562fdeebad28e9ec5
Harold Ramos <chattingharold@gmail.com>
2018-09-13 00:22:30 -0400

    Overloading operators

---
+++ 25-Operators/02-overloading-operators/code.swift
@@ -0,0 +1,8 @@
+// Operators overloading 
+// Fancy way of saying adding same types of values
+
+var num1 = 10
+var num2 = 20 + num1
+
+var mondayInventory = ["eggs", "straberries"]
+var tuesdayInventory = ["sugar", "beans"] + mondayInventory
