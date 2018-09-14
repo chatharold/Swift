426e5152008d46625da3ca3821a5a4a97b6f795a
Harold Ramos <chattingharold@gmail.com>
2018-09-14 00:19:31 -0400

    break

---
+++ 10-Loops/06-break/code.swift
@@ -0,0 +1,11 @@
+var seconds = 10
+
+while seconds >= 1 {
+    print(seconds)
+    
+    if seconds == 3 {
+        print("The device has been deactivated!")
+        break
+    }
+    seconds -= 1
+}
