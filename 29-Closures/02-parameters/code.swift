af1f09652b34609e3aee1e1093accea87cac0232
Harold Ramos <chattingharold@gmail.com>
2018-09-16 19:23:32 -0400

    returned-values

--- 29-Closures/02-parameters/code.swift
+++
@@ -1,6 +0,0 @@
-let greet = {
-    (firstName: String, lastName: String) in
-    print("Ladies and Gentleman, let's welcome \"\(firstName) \(lastName)\".")
-}
-
-greet("Harold", "Ramos")
--- 29-Closures/code.swift
+++
@@ -1,5 +0,0 @@
-let greet = {
-    print("Welcome Home!")
-}
-
-greet()
