866538b4e6bb29b44f1b18a9bb9937e796b48b45
Harold Ramos <chattingharold@gmail.com>
2018-09-16 18:56:35 -0400

    multiple-returns

---
+++ 11-Functions/07-multiple-returns/code.swift
@@ -0,0 +1,9 @@
+func fullName(first: String, last: String) -> (String, String) {
+    return (first, last)
+}
+
+let full = fullName(first: "Harold", last: "Ramos")
+
+
+print(full.0)
+print(full.1)
