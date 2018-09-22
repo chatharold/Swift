15844e4af9247c35e296493682a241d191f0d2a3
Harold Ramos <chattingharold@gmail.com>
2018-09-21 21:27:18 -0400

    update

--- 24-Structs/07-property-observers/code.swift
+++ 24-Structs/07-property-observers/code.swift
@@ -1,3 +1,4 @@
+// Ex1
 struct Car {
     var moveCar: Int = 0 {
         willSet {
@@ -15,3 +16,22 @@ var start = Car()
 start.moveCar = 100
 start.moveCar = 500
 start.moveCar = 600
+
+
+
+
+// Ex2
+struct Battery {
+    var device: String
+    var percentage: Int {
+        didSet {
+            print("My \(device) has a battery life of \(percentage)%.")
+        }
+    }
+}
+
+
+var iphone = Battery(device: "Iphone", percentage: 0)
+
+iphone.percentage = 30
+iphone.percentage = 56
