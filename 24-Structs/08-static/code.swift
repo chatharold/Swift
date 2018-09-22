63dc3fadc4df992b7b423f1a334f0732fd37b04a
Harold Ramos <chattingharold@gmail.com>
2018-09-22 17:17:59 -0400

    update static

--- 24-Structs/08-static/code.swift
+++ 24-Structs/08-static/code.swift
@@ -1,4 +1,5 @@
 // Creates a constant property
+// Ex1
 struct Person {
     static var fullName = "Harold Ramos"
 }
@@ -6,4 +7,23 @@ struct Person {
 let harold = Person.fullName
 harold
 
+
+// Ex2
+struct Person {
+    
+    static var age = 2018
+    
+    var fullName: String
+    
+    init(firstName: String, lastName: String) {
+        fullName = "\(firstName) \(lastName)"
+    }
+    
+}
+
+let harold = Person(firstName: "Harold", lastName: "Ramos")
+
+print("My name is = \(harold.fullName)")
+
+print("My age = \(Person.age)")
 
