9f0743f5c79321ed2c6126e02cf1e1ac1c6f7a1f
Harold Ramos <chattingharold@gmail.com>
2018-09-24 23:21:02 -0400

    extension

---
+++ 30-Protocols/03-intro-extension/code.swift
@@ -0,0 +1,22 @@
+// ex1
+extension Int {
+    func squared() -> Int {
+        return self * self
+    }
+}
+
+let number = 4
+number.squared()
+
+
+
+// ex1
+extension Int {
+    func even() -> Bool {
+        return self % 2 == 0
+    }
+}
+
+
+let number2 = 4
+number2.even()
