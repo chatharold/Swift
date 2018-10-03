500c389639778a68bebaf0d7884444920cfe42fa
Harold Ramos <chattingharold@gmail.com>
2018-10-03 06:42:37 -0400

    updating AVFoundation

--- 000-API/01-AVFoundation/code.swift
+++ 000-API/01-AVFoundation/code.swift
@@ -3,23 +3,27 @@ import AVFoundation
 
 class ViewController: UIViewController{
     
-    // Intances
+    // create audio player
     var audioPlayer: AVAudioPlayer?
    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
+        // create path
         let path = Bundle.main.path(forResource: "note\(sender.tag).wav", ofType: nil)!
+        
+        // add path to URL
         let url = URL(fileURLWithPath: path)
         
         do {
+            // add url to audio player
             audioPlayer = try AVAudioPlayer(contentsOf: url)
             audioPlayer?.play()
         }
         catch {
+            // error loading the url to audio player
             print("File could not load")
         }
     }
