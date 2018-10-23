4cd4024a315eced4091e0f30300bac5d8a55f1e8
Harold Ramos <chattingharold@gmail.com>
2018-10-22 22:35:32 -0400

    Updating designated init

--- 18-Classes/09-init/02-designated_convenience/code.swift
+++ 18-Classes/09-init/02-designated_convenience/code.swift
@@ -3,10 +3,11 @@ class Car {
     var year: Int = 0
     var color: String = "Black"
     
-    // Designated init()
-    init() {
+    // Designated init() no need to call init
+    // Its been called on line 14
+    // init() {
         
-    }
+    // }
     
     // Convenience init()
     convenience init (Year: Int, Color: String) {
